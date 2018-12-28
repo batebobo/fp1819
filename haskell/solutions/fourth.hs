74e52bfedfdf431773f13d6bb2591f38b35781b7
Boyan Vushkov <bvushkov@asteasolutions.com>
2018-12-28 16:25:11 +0200

    Update fourth week solutions

--- haskell/solutions/fourth.hs
+++ haskell/solutions/fourth.hs
@@ -1,12 +1,26 @@
+{-# LANGUAGE ParallelListComp #-}
+
 myMap :: (a -> b) -> [a] -> [b]
 myMap _ [] = []
 myMap f (x : xs) = f x : myMap f xs
+
+myMap' :: (a -> b) -> [a] -> [b]
+myMap' f = foldl (\acc x -> (f x) : acc) []
+
+myMap'' :: (a -> b) -> [a] -> [b]
+myMap'' f xs = [(f x) | x <- xs]
 
 myFilter :: (a -> Bool) -> [a] -> [a]
 myFilter _ [] = []
 myFilter p (x : xs)
     | p x = x : myFilter p  xs
     | otherwise = myFilter p xs
+
+myFilter' :: (a -> Bool) -> [a] -> [a]
+myFilter' p = foldl (\acc x -> if (p x) then x : acc else acc) []
+
+myFilter'' :: (a -> Bool) -> [a] -> [a]
+myFilter'' p xs = [x | x <- xs, p x]
 
 
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
@@ -19,9 +33,14 @@ myZipWith _ _ [] = []
 myZipWith _ [] _ = []
 myZipWith f (x : xs) (y : ys) = f x y : myZipWith f xs ys
 
+-- това не се поддръжа директно в езика. трябва ни
+-- {-# LANGUAGE ParallelListComp #-}
+-- най-горе във файлa
+myZipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith' f xs ys = [(f x y) | x <- xs | y <- ys]
 
 moreLists :: [a] -> [a] -> [[a]]
-moreLists = myZipWith (\x y -> x:y:[])
+moreLists = myZipWith (\x y -> [x,y]) -- алтернатива - (\x y -> x:y:[])
 
 myTakeWhile :: (a -> Bool) -> [a] -> [a]
 myTakeWhile _ [] = []
